385e9d482aceed44ac3386c33c5ea5753d370ec3
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2024-12-26 11:16:31 +0530

    Initial commit

---
+++ src/main/java/com/uttam/JopApp/JopAppApplication.java
@@ -0,0 +1,13 @@
+package com.uttam.JopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttam/JopApp/JopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttam.JopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
